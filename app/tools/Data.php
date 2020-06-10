9cec7ad87c5e2feb443dbd4af92c6caae659c10c
radynje <radynje@gmail.com>
2020-06-10 18:41:50 +0300

    Automatic database deployment

--- app/tools/Data.php
+++ app/tools/Data.php
@@ -2,12 +2,67 @@
 
 namespace tools;
 
+use mysqli;
+
 class Data {
-    public static function schemaToSQL($schema) {
-        $sql;
+    private $connect;
 
-        foreach ($schema as $table) {
+    public function __construct(
+        string $host,
+        string $user,
+        string $password,
+        string $name = null
+    ) {
+        $this->connect = new mysqli(
+            $host,
+            $user,
+            $password,
+            $name
+        );
 
+        if ($this->connect->connect_error) die("Соединениться не удалось: {$this->connect->connect_error}");
+
+        $this->connect->set_charset('utf8');
+    }
+
+    public function close() : void {
+        $this->connect->close();
+    }
+
+    public function query($sql) : void {
+        if (!$this->connect->query($sql)) echo "Ошибка MySQL: {$this->connect->error}";
+    }
+
+    public function setDatabase(string $name) : void {
+        $this->query("CREATE DATABASE IF NOT EXISTS `{$name}` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+        $this->connect->select_db($name);
+    }
+
+    public function createTable(
+        string $name,
+        array $schema,
+        string $engine = 'InnoDB'
+    ) : void {
+        $this->query("CREATE TABLE IF NOT EXISTS `{$name}` " . Data::schemaToSQL($schema) . " ENGINE = {$engine}; \n\n");
+    }
+
+    private static function schemaToSQL(array $schema) : string {
+        $SQLSchema = '';
+        $primary = '';
+
+        foreach ($schema as $title => $data) {
+            $columnSQL = "`{$title}`";
+
+            $columnSQL .= ' ' . strtoupper($data['type']);
+            $columnSQL .= !empty($data['length']) ? "({$data['length']})" : '';
+            $columnSQL .= empty($data['null']) ? ' NOT NULL' : ' NULL';
+            $columnSQL .= !empty($data['auto']) ? ' AUTO_INCREMENT' : '';
+
+            if (!empty($data['primary'])) $primary = "PRIMARY KEY (`{$title}`)";
+
+            $SQLSchema .= $columnSQL . ',';
         }
+
+        return "({$SQLSchema} {$primary})";
     }
 }
